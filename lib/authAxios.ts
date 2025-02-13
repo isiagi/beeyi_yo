39dea3be8ba2bca246373174114a9e965f9c00eb
isiagi <isiagigeofrey0@gmail.com>
2025-02-13 19:14:44 +0300

    fix auth

--- lib/authAxios.ts
+++ lib/authAxios.ts
@@ -1,11 +1,34 @@
 import axios from "axios";
 
-const authInstance = axios.create({
-  baseURL: "http://localhost:8000/api",
-  headers: {
-    "Content-Type": "multipart/form-data",
-    Authorization: `Token ${localStorage.getItem("token")}`,
-  },
-});
+const createAuthInstance = () => {
+  // Create the axios instance
+  const instance = axios.create({
+    baseURL: "http://localhost:8000/api",
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+  // Add request interceptor to dynamically add token
+  instance.interceptors.request.use(
+    (config) => {
+      // Only try to get token if window is defined (client-side)
+      if (typeof window !== "undefined") {
+        const token = localStorage.getItem("token");
+        if (token) {
+          config.headers.Authorization = `Token ${token}`;
+        }
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  return instance;
+};
+
+const authInstance = createAuthInstance();
 
 export default authInstance;
